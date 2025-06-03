812fc0e666470d9a5b786d69a7dfda393cad20ae
Ghost9841 <asubedi778@gmail.com>
2025-06-03 19:40:16 +0545

    Component Made for Todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import Navbar from "./layout/navbar/Navbar"
 import SideBar from "./layout/sidebar/page/SideBar"
+import TodoPage from './features/todo/pages/TodoPage';
+
 
 
 
@@ -10,9 +13,15 @@ function App() {
     <>
       <main className="flex bg-gray-200 text-gray-900 w-full min-h-screen">
         <SideBar/>
-        <nav className="ml-60 flex flex-col w-full h-full py-7 px-9 bg-gray-200 ${isCollapsed ? 'ml-0 md:ml-16' : 'ml-72 md:ml-64' }">
+        <section className="ml-60 flex flex-col w-full h-full py-7 px-9 bg-gray-200 ${isCollapsed ? 'ml-0 md:ml-16' : 'ml-72 md:ml-64' }">
           <Navbar />
-        </nav>
+        <BrowserRouter>
+           <Routes>
+            <Route path={'/todo'} element={<TodoPage/>}  />
+          </Routes>
+        </BrowserRouter>
+        </section>
+       
       </main>
     </>
   )
